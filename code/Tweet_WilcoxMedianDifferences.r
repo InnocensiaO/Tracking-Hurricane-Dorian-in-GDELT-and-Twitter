549473984464f252ac2e6ec3bdcccae132edf8c3
InnocensiaO <40710381+InnocensiaO@users.noreply.github.com>
2020-07-09 20:25:59 -0400

    Update Tweet_WilcoxMedianDifferences.r

--- code/Tweet_WilcoxMedianDifferences.r
+++ code/Tweet_WilcoxMedianDifferences.r
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 setwd("/TweetProximityData") #set the working dir
 #Source data is in the TweetProximityData
 options(max.print=1000000)
@@ -131,5 +130,3 @@ medSept4_Sept5<-wilcox.test(TrackpointSept6_12PMTweetSept4, TrackpointSept6_12PM
 medSept5_Sept6<-wilcox.test(TrackpointSept6_12PMTweetSept5, TrackpointSept6_12PMTweetSept6, mu=0, alt = "two.sided", paired = F, conf.int = T, conf.level = 0.99, exact = F, correct = F)
 
 
-
->>>>>>> 89007dfe6c10759cd82988c94da3787e561955ec
